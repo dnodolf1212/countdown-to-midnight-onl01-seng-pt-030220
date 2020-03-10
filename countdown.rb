8404cfb717c21be9e82ca17465dd4216e1953233
Dwight Nodolf <dnodolf@gmail.com>
2020-03-10 04:20:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 #input --> integers in seconds
 #output --> a string ' #SECONDS' with the seconds counted interpolated into each looped string
 
-count = 10    
+    
 def countdown(seconds)
   
   while seconds > 0

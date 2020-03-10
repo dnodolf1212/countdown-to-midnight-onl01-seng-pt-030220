76bd128b2beafbba158807660ead1e3b4d8ded38
Dwight Nodolf <dnodolf@gmail.com>
2020-03-10 04:38:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -17,7 +17,9 @@ end
 def countdown_with_sleep(seconds)
   while seconds > 0 
   puts "#{seconds} SECOND(S)!"
+  sleep(1.seconds) 
   seconds -= 1 
   
 end
-return "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!" 
+end

54f235204d5cc9b63968068be10971f3e3466bf8
Dwight Nodolf <dnodolf@gmail.com>
2020-03-10 03:31:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,11 @@
 #input --> integers in seconds
 #output --> a string ' #SECONDS' with the seconds counted interpolated into each looped string
 
-count = 16   
-def countdown(number)
+count = 10    
+def countdown
   count -= 1
-  while count > 12   
-   return "#{number} SECOND(S)!"
+  while count >= 12   
+   return "#{count} SECOND(S)!"
 end
 
    return "HAPPY NEW YEAR!"
